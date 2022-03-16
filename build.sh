99e7641a70ecfedd022cd46ed876e8fda0d32d2d
MilkyDeveloper <48262994+MilkyDeveloper@users.noreply.github.com>
2022-03-16 16:22:20 -0500

    Overhaul build.sh so devs can work on multiple kernels at once

--- build.sh
+++ build.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-echo "What kernel version would you like?"
+# Exit on errors
+set -e
+
+echo "What kernel version would you like? (choose a number 1-3)"
 echo "This can be any ChromeOS kernel branch, but the recommended versions are:"
-printf "chromeos-5.10:\n  (Kernel version that is up-to-date and has good audio support with SOF)\n"
-printf "alt-chromeos-5.10:\n  (Kernel version that is up-to-date but is pinned to a commit that supports KBL/SKL devices which do not support SOF)\n"
-printf "chromeos-5.4:\n  (Often causes more issues despite being a commonly-used kernel in ChromeOS)\n"
+printf "1: chromeos-5.10:\n  (Kernel version that is up-to-date and has good audio support with SOF)\n"
+printf "2: alt-chromeos-5.10:\n  (Kernel version that is up-to-date but is pinned to a commit that supports KBL/SKL devices which do not support SOF)\n"
+printf "3: chromeos-5.4:\n  (Often causes more issues despite being a commonly-used kernel in ChromeOS)\n"
+
 read KERNEL_VERSION
+
+case $KERNEL_VERSION in
+    "1"|"chromeos-5.10")     KERNEL_VERSION="chromeos-5.10"     ;;
+    "2"|"alt-chromeos-5.10") KERNEL_VERSION="alt-chromeos-5.10" ;;
+    "3"|"chromeos-5.4")      KERNEL_VERSION="chromeos-5.4"      ;;
+    *) echo "Please supply a valid kernel version"; exit ;;
+esac
 
 echo "Cloning kernel $KERNEL_VERSION"
 
 # Latest commit tested for 5.10: 142ef9297957f6df8a08f75d772ae8a5448c6f6c
 
 if [[ $KERNEL_VERSION == "alt-chromeos-5.10" ]]; then
-    git clone --branch $KERNEL_VERSION --single-branch https://chromium.googlesource.com/chromiumos/third_party/kernel.git
+    git clone --branch $KERNEL_VERSION --single-branch https://chromium.googlesource.com/chromiumos/third_party/kernel.git $KERNEL_VERSION
     git checkout $(git rev-list -n 1 --first-parent --before="2021-08-1 23:59" $KERNEL_VERSION)
 else
-    git clone --branch $KERNEL_VERSION --single-branch --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel.git
+    git clone --branch $KERNEL_VERSION --single-branch --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel.git $KERNEL_VERSION || true
 fi
 
-cd kernel
+cd $KERNEL_VERSION
 
 echo "Patching the kernel"
-git apply ../patches/*
+git apply ../patches/* || true
 echo "mod" >> .gitignore
 touch .scmversion
 
